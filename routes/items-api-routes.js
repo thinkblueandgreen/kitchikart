aeac1c244998bb9f12283272e8855e4ee7ddbb7b
Mathi <arline.mathivadhani@gmail.com>
2020-01-16 12:20:55 -0800

    BuyProduct page added

--- routes/items-api-routes.js
+++ routes/items-api-routes.js
@@ -43,9 +43,38 @@ module.exports = function(app) {
 
   // POST route for saving a new post
   app.post("/api/posts", function(req, res) {
-    db.kitckenitems.create(req.body).then(function(dbkitckenitems) {
+    var productName = req.body.productName;
+    var productDescription = req.body.productDescription;
+    var picurl = req.body.picurl;
+    var category = req.body.category;
+
+    /*db.kitckenitems.build({ 
+      productName: productName, 
+      productDescription: productDescription, 
+      picurl: picurl,
+      category: category
+    })
+    .save()
+    .then(anotherTask => {
+      console.log('the data saved!');
+      // you can now access the currently saved task with the variable anotherTask... nice!
+    })
+    .catch(error => {
+      console.log('uh oh something wasn't right!');
+      console.log(error);
+      // Ooops, do some error-handling
+    });*/
+
+    console.log("--> req.body"+req.body);
+    /*db.kitckenitems.create({category: `${category}`, productName:`${productName}`, picurl:`${picurl}`, productDescription: `${productDescription}`, quantity: Math.floor(Math.random()*i*10 )}).then(function(dbkitckenitems) {
+   
+     res.json(dbkitckenitems);
+    });*/
+
+      db.kitchenitems.create({category: categories[3], productName:"productName", picurl:"picurl", productDescription: "productDescription", quantity: Math.floor(Math.random() )}).then(data=>{console.log(data)})
       res.json(dbkitckenitems);
-    });
+
+
   });
 
   app.post('/api/createDummyData', (req,res)=>{
@@ -102,6 +131,11 @@ app.get("/buy/:id", function(req, res) {
     res.render("home");
 });
 
+
+app.get("/addproduct", function(req, res) {
+  console.log("adding product to the db functionality is been done");
+  res.render("addProduct");
+});
 
 };
 
